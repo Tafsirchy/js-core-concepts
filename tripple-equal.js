09020569649240cf50b590ea8475023332964e12
Tafsirchy <tafsirchy1000@gmail.com>
2025-09-05 23:34:16 +0600

    tripple equal

--- tripple-equal.js
+++ tripple-equal.js
@@ -1,3 +1,6 @@
+//first check the type. if type is different then they are different and false
+
+// if type is same then check the value. if value is same then they are same and true, if value is false then they are false
 console.log(2 === 2);
 console.log(2 === '2');
 console.log(1 === 2);
