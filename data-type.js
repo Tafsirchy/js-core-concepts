848785210da41ac9829370c7b2448f2e76db6bbc
Tafsirchy <tafsirchy1000@gmail.com>
2025-09-05 21:59:24 +0600

    js data type

---
+++ data-type.js
@@ -0,0 +1,27 @@
+// js is a dynamic type programing language
+let data = 4;
+data = 'tormuj';
+data = false;
+data = {name: 'bangi', price: 40};
+data = [1,2,3,4,5];
+data = undefined;
+data = null;
+console.log(typeof data);
+
+// js premetive data type
+const price = 50;
+const name = 'tormuj';
+const isHappy = true;
+const inKnown; // undefined
+const why = undefined // usually you should not set value to undefined
+const married = null // null is an object
+
+// non-premetive data type
+const student = {
+    name: 'pakhi',
+    age: 20,
+    location: 'dhaka',
+    subject: 'zology',
+}
+
+const friends = ['tom', 'jerry', 'harry', 'jason']
